e117f97961cad09a52b00170d5f000dc18612256
unknown <awesomecode123@outlook.com>
2024-12-18 10:13:57 -0800

    concide variable name

--- app_tkinter_1.py
+++ app_tkinter_1.py
@@ -64,97 +64,131 @@ class JRCurveFitApp:
         run_image = PhotoImage(file="run.png")
         run2_image = PhotoImage(file="run2.png")
         
-        new_button = Button(toolbar_frame, image=new_image, command=lambda: print("Open button clicked"))
+        new_button = Button(toolbar_frame, image=new_image, command=self.new_analysis)
         new_button.image = new_image
         new_button.pack(side=tk.LEFT)
 
-        open_button = Button(toolbar_frame, image=open_image, command=lambda: print("Open button clicked"))
+        open_button = Button(toolbar_frame, image=open_image, command=self.open_analysis)
         open_button.image = open_image
         open_button.pack(side=tk.LEFT)
 
-        save_button = Button(toolbar_frame, image=save_image, command=lambda: print("save button clicked"))
+        save_button = Button(toolbar_frame, image=save_image, command=self.save_analysis)
         save_button.image = save_image
         save_button.pack(side=tk.LEFT)
         
-        run_button = Button(toolbar_frame, image=run_image, command=lambda: print("run button clicked"))
+        run_button = Button(toolbar_frame, image=run_image, command=self.run_analysis)
         run_button.image = run_image
         run_button.pack(side=tk.LEFT)
 
-        run2_button = Button(toolbar_frame, image=run2_image, command=lambda: print("run2 button clicked"))
+        run2_button = Button(toolbar_frame, image=run2_image, command=self.run2_analysis)
         run2_button.image = run2_image
         run2_button.pack(side=tk.LEFT)
 
         pipe_frame = ttk.LabelFrame(first_col, text="Pipe & Loads", padding="10")
         pipe_frame.grid(row=0, column=0, sticky="nw", padx=10, pady=10)
 
-        fields = [
-            ("Nominal Pipe Size (in)", "12"), 
-            ("Pipe OD (in)", "12"), 
-            ("Wall Thickness (in)", "0.5"),
-            ("Pressure (psi)", "2250"), 
-            ("Temperature (°F)", "600"), 
-            ("Max. Op. Stress (ksi)", "15")
-        ]
+        ttk.Label(pipe_frame, text="Nominal Pipe Size (in)").grid(row=0, column=0, sticky="w", padx=5, pady=2)
+        Normal_Pipe_Size = ttk.Entry(pipe_frame, width=10)
+        Normal_Pipe_Size.insert(0, 12)
+        Normal_Pipe_Size.grid(row=0, column=1, padx=5, pady=2)
+        self.Normal_Pipe_Size = Normal_Pipe_Size
 
-        for i, (label, default) in enumerate(fields):
-            ttk.Label(pipe_frame, text=label).grid(row=i, column=0, sticky="w", padx=5, pady=2)
-            entry = ttk.Entry(pipe_frame, width=10)
-            entry.insert(0, default)
-            entry.grid(row=i, column=1, padx=5, pady=2)
+        ttk.Label(pipe_frame, text="Pipe OD (in)").grid(row=1, column=0, sticky="w", padx=5, pady=2)
+        Pipe_OD = ttk.Entry(pipe_frame, width=10)
+        Pipe_OD.insert(0, 12)
+        Pipe_OD.grid(row=1, column=1, padx=5, pady=2)
+        self.Pipe_OD = Pipe_OD
+
+        ttk.Label(pipe_frame, text="Wall Thickness (in)").grid(row=2, column=0, sticky="w", padx=5, pady=2)
+        Wall_Thickness = ttk.Entry(pipe_frame, width=10)
+        Wall_Thickness.insert(0, 0.5)
+        Wall_Thickness.grid(row=2, column=1, padx=5, pady=2)
+        self.Wall_Thickness = Wall_Thickness
+
+        ttk.Label(pipe_frame, text="Pressure (psi)").grid(row=3, column=0, sticky="w", padx=5, pady=2)
+        Pressure = ttk.Entry(pipe_frame, width=10)
+        Pressure.insert(0, 2250)
+        Pressure.grid(row=3, column=1, padx=5, pady=2)
+        self.Pressure = Pressure
+
+        ttk.Label(pipe_frame, text="Temperature (°F)").grid(row=4, column=0, sticky="w", padx=5, pady=2)
+        Temperature = ttk.Entry(pipe_frame, width=10)
+        Temperature.insert(0, 600)
+        Temperature.grid(row=4, column=1, padx=5, pady=2)
+        self.Temperature = Temperature
+
+        ttk.Label(pipe_frame, text="Max. Op. Stress (ksi)").grid(row=5, column=0, sticky="w", padx=5, pady=2)
+        Max_Op_Stress = ttk.Entry(pipe_frame, width=10)
+        Max_Op_Stress.insert(0, 15)
+        Max_Op_Stress.grid(row=5, column=1, padx=5, pady=2)
+        self.Max_Op_Stress = Max_Op_Stress
 
         # BAC section
         bac_frame = ttk.LabelFrame(first_col, text="BAC", padding="10")
         bac_frame.grid(row=1, column=0, sticky="w", padx=10, pady=10)
 
         ttk.Label(bac_frame, text="Max. Op. Stress (ksi)").grid(row=0, column=0, sticky="w", padx=5)
-        bac_entry = ttk.Entry(bac_frame, width=10)
-        bac_entry.insert(0, "55")
-        bac_entry.grid(row=0, column=1)
+        BAC_Max_Op_Stress = ttk.Entry(bac_frame, width=10)
+        BAC_Max_Op_Stress.insert(0, "55")
+        BAC_Max_Op_Stress.grid(row=0, column=1)
+        self.BAC_Max_Op_Stress = BAC_Max_Op_Stress
 
         ttk.Label(bac_frame, text="No. of Points").grid(row=1, column=0, sticky="w", padx=5)
-        bac_entry = ttk.Entry(bac_frame, width=10)
-        bac_entry.insert(0, "15")
-        bac_entry.grid(row=1, column=1)
+        BAC_No_Of_Points = ttk.Entry(bac_frame, width=10)
+        BAC_No_Of_Points.insert(0, "15")
+        BAC_No_Of_Points.grid(row=1, column=1)
+        self.BAC_No_Of_Points = BAC_No_Of_Points
 
         # Radio Buttons
-        bac_option = tk.StringVar(value="Biased")
-        ttk.Radiobutton(bac_frame, text="Equidistant", variable=bac_option, value="Equidistant").grid(row=2, column=0, sticky="w")
-        ttk.Radiobutton(bac_frame, text="Biased", variable=bac_option, value="Biased").grid(row=2, column=1, sticky="w")
+        BAC_option = tk.StringVar(value="Biased")
+        ttk.Radiobutton(bac_frame, text="Equidistant", variable=BAC_option, value="Equidistant").grid(row=2, column=0, sticky="w")
+        ttk.Radiobutton(bac_frame, text="Biased", variable=BAC_option, value="Biased").grid(row=2, column=1, sticky="w")
+        self.BAC_option = BAC_option
 
         options = ["1-ASME SA-106 Gr C", "2-ASME SA-312(Type 304/304L)", "3-ASME SA-106 Gr B", "4-ASME SA-335 Gr P11", "5-User Defined"]
-        variable = tk.StringVar(root)
-        variable.set(options[2])  # default value
-        optionmenu = tk.OptionMenu(first_col, variable, *options)
+        TypeOption= tk.StringVar(root)
+        TypeOption.set(options[2])  # default value
+        optionmenu = tk.OptionMenu(first_col, TypeOption, *options)
         optionmenu.grid(row=2, column=0, sticky="w", padx=10, pady=10)
+        self.TypeOption = TypeOption
 
-        # menubutton = Menubutton(first_col, text="2-ASME SA-312(Type 304/304L)")
-        # menubutton.grid(row=2, column=0, sticky="w", padx=10, pady=10)
-        # menu = Menu(menubutton, tearoff=0)
-        # menu.add_command(label="1-ASME SA-106 Gr C")
-        # menu.add_command(label="2-ASME SA-312(Type 304/304L)")
-        # menubutton.config(menu=menu)
-
-        # tensile_frame = ttk.LabelFrame(main_frame, text="2 - ASME SA-312 (Type 304/304L)", padding="10")
         tensile_frame = ttk.LabelFrame(first_col, text="Tensile Properties", padding="10")
 
         tensile_frame.grid(row=3, column=0, sticky="w", padx=10, pady=10)
+        
+        ttk.Label(tensile_frame, text="Yield Strength (ksi)").grid(row=0, column=0, sticky="w", padx=5, pady=2)
+        Yield_Strength = ttk.Entry(tensile_frame, width=10)
+        Yield_Strength.insert(0, "30")
+        Yield_Strength.grid(row=0, column=1, padx=5, pady=2)
+        self.Yield_Strength = Yield_Strength
 
-        tensile_fields = [
-            ("Yield Strength (ksi)", "30"), 
-            ("UTS (ksi)", "60"),
-            ("Elastic Modulus (ksi)", "28000"),
-            ("RO Alpha", "0.5"), 
-            ("RO n", "3.5")
-        ]
-        for i, (label, default) in enumerate(tensile_fields):
-            ttk.Label(tensile_frame, text=label).grid(row=i, column=0, sticky="w", padx=5, pady=2)
-            entry = ttk.Entry(tensile_frame, width=10)
-            entry.insert(0, default)
-            entry.grid(row=i, column=1, padx=5, pady=2)
+        ttk.Label(tensile_frame, text="UTS (ksi)").grid(row=1, column=0, sticky="w", padx=5, pady=2)
+        UTS = ttk.Entry(tensile_frame, width=10)
+        UTS.insert(0, "60")
+        UTS.grid(row=1, column=1, padx=5, pady=2)
+        self.UTS = UTS
+
+        ttk.Label(tensile_frame, text="Elastic Modulus (ksi)").grid(row=2, column=0, sticky="w", padx=5, pady=2)
+        Elastic_Modulus = ttk.Entry(tensile_frame, width=10)
+        Elastic_Modulus.insert(0, "28000")
+        Elastic_Modulus.grid(row=2, column=1, padx=5, pady=2)
+        self.Elastic_Modulus = Elastic_Modulus
+
+        ttk.Label(tensile_frame, text="RO Alpha").grid(row=3, column=0, sticky="w", padx=5, pady=2)
+        RO_Alpha = ttk.Entry(tensile_frame, width=10)
+        RO_Alpha.insert(0, "4.5")
+        RO_Alpha.grid(row=3, column=1, padx=5, pady=2)
+        self.RO_Alpha = RO_Alpha
+
+        ttk.Label(tensile_frame, text="RO n").grid(row=4, column=0, sticky="w", padx=5, pady=2)
+        RO_n = ttk.Entry(tensile_frame, width=10)
+        RO_n.insert(0, "3.5")
+        RO_n.grid(row=4, column=1, padx=5, pady=2)
+        self.RO_n = RO_n
 
         # Buttons
-        ttk.Button(first_col, text="Fit R-O Curve from Stress-Strain Data").grid(row=4, column=0, pady=5)
-        ttk.Button(first_col, text="      Fit R-O Curve from YS-UTS      ").grid(row=5, column=0, pady=5)
+        ttk.Button(first_col, text="Fit R-O Curve from Stress-Strain Data", command=self.Fit_ROCurve_from_Stress_Strain_Data).grid(row=4, column=0, pady=5)
+        ttk.Button(first_col, text="      Fit R-O Curve from YS-UTS      ", command=self.Fit_ROCurve_from_YS_UTS).grid(row=5, column=0, pady=5)
 
         # ========== Section 3: Crack Morphology ==========
         second_col = ttk.Frame(input_frame)
@@ -162,7 +196,7 @@ class JRCurveFitApp:
         crack_frame = ttk.LabelFrame(second_col, text="Crack Morphology", padding="10")
         crack_frame.grid(row=0, column=0, sticky="nw", padx=10, pady=10)
 
-        CrackMorphology = tk.StringVar(value="CrackMorphology")
+        CrackMorphology = tk.StringVar(value="Fatigue")
 
         options = [
             "IGSCC", "PWSCC Weld", "User-Defined", 
@@ -174,47 +208,69 @@ class JRCurveFitApp:
             "Cor. Fatigue", "Fatigue"
         ]
 
-        
         for i, opt in enumerate(options):
             ttk.Radiobutton(crack_frame, text=opt, variable=CrackMorphology, value=opt).grid(row=i, column=1, sticky="w", pady=2)
 
+        self.CrackMorphology = CrackMorphology
+
         ttk.Label(crack_frame, text="45 Degree Turns").grid(row=3, column=0, sticky="w", padx=5, pady=2)
-        entry = ttk.Entry(crack_frame, width=10)
-        entry.grid(row=3, column=1, padx=5, pady=2)
+        Degree_Turns = ttk.Entry(crack_frame, width=10)
+        Degree_Turns.grid(row=3, column=1, padx=5, pady=2)
+        self.Degree_Turns = Degree_Turns
 
         ttk.Label(crack_frame, text="Roughness").grid(row=4, column=0, sticky="w", padx=5, pady=2)
-        entry = ttk.Entry(crack_frame, width=10)
-        entry.grid(row=4, column=1, padx=5, pady=2)
+        Roughness = ttk.Entry(crack_frame, width=10)
+        Roughness.grid(row=4, column=1, padx=5, pady=2)
+        self.Roughness = Roughness
 
         GPM_frame = ttk.LabelFrame(second_col, text="Detectable Leak Rates (GPM)", padding="10")
         GPM_frame.grid(row=1, column=0, sticky="w", padx=10, pady=10)
-        leak_rates = ["0.05", "0.1", "0.25", "0.5", "1.0"]
-        for i, rate in enumerate(leak_rates):
-            ttk.Radiobutton(GPM_frame, text=rate, value=rate).grid(row=0, column=i, sticky="w")
+        GPM_0 = ttk.Entry(GPM_frame, width=5)
+        GPM_0.insert(0, 0.05)
+        GPM_0.grid(row=0, column=0, sticky="w",padx=3)
+        self.GPM_0 = GPM_0
+        GPM_1 = ttk.Entry(GPM_frame, width=5)
+        GPM_1.grid(row=0, column=1, sticky="w", padx=3)
+        GPM_1.insert(0, 0.1)
+        self.GPM_1 = GPM_1
+        GPM_2 = ttk.Entry(GPM_frame, width=5)
+        GPM_2.grid(row=0, column=2, sticky="w",padx=3)
+        GPM_2.insert(0, 0.25)
+        self.GPM_2 = GPM_2
+        GPM_3 = ttk.Entry(GPM_frame, width=5)
+        GPM_3.grid(row=0, column=3, sticky="w",padx=3)
+        GPM_3.insert(0, 0.5)
+        self.GPM_3 = GPM_3
+        GPM_4 = ttk.Entry(GPM_frame, width=5)
+        GPM_4.grid(row=0, column=4, sticky="w",padx=3)
+        GPM_4.insert(0, 1.0)
+        self.GPM_4 = GPM_4
 
         # Fracture Toughness
         fracture_frame = ttk.LabelFrame(second_col, text="Fracture Toughness", padding="10")
         fracture_frame.grid(row=2, column=0, sticky="w", padx=10, pady=10)
         ttk.Label(fracture_frame, text="Coef. C (-1/R):").grid(row=0, column=0, sticky="w")
-        c_entry = ttk.Entry(fracture_frame, width=10)
-        c_entry.insert(0, "1.5")
-        c_entry.grid(row=0, column=1, padx=5)
+        Coef_C_J_R = ttk.Entry(fracture_frame, width=10)
+        Coef_C_J_R.insert(0, "1.5")
+        Coef_C_J_R.grid(row=0, column=1, padx=5)
+        self.Coef_C_J_R = Coef_C_J_R
 
         ttk.Label(fracture_frame, text="Exponent m:").grid(row=1, column=0, sticky="w")
-        m_entry = ttk.Entry(fracture_frame, width=10)
-        m_entry.insert(0, "0.43")
-        m_entry.grid(row=1, column=1, padx=5)
+        Exponent_m = ttk.Entry(fracture_frame, width=10)
+        Exponent_m.insert(0, "0.43")
+        Exponent_m.grid(row=1, column=1, padx=5)
+        self.Exponent_m = Exponent_m
 
-        ttk.Button(fracture_frame, text="Fit J-R Curve from Data").grid(row=3, column=0, columnspan=2, pady=5)
+        ttk.Button(fracture_frame, text="Fit J-R Curve from Data", command=self.Fit_JRCurve_from_Data).grid(row=3, column=0, columnspan=2, pady=5)
 
         # ========== Section 4: Failure Criteria ==========
         failure_frame = ttk.LabelFrame(second_col, text="Failure Criteria", padding="10")
         failure_frame.grid(row=3, column=0, columnspan=2, sticky="w", padx=10, pady=10)
 
-        failure_option = tk.StringVar(value="Limit Load")
-        ttk.Radiobutton(failure_frame, text="Limit Load", variable=failure_option, value="Limit Load").grid(row=0, column=0, sticky="w")
-        ttk.Radiobutton(failure_frame, text="EPFM", variable=failure_option, value="EPFM").grid(row=0, column=1, sticky="w")
-
+        Failure_Criteria = tk.StringVar(value="Limit Load")
+        ttk.Radiobutton(failure_frame, text="Limit Load", variable=Failure_Criteria, value="Limit Load").grid(row=0, column=0, sticky="w")
+        ttk.Radiobutton(failure_frame, text="EPFM", variable=Failure_Criteria, value="EPFM").grid(row=0, column=1, sticky="w")
+        self.Failure_Criteria = Failure_Criteria
         
         # ========== Section 5: Output ==========
         # Table Frame
@@ -255,6 +311,39 @@ class JRCurveFitApp:
         # Results Label
         self.result_label = ttk.Label(plot_frame, text="", font=("Arial", 12))
         self.result_label.pack(side=tk.BOTTOM, pady=10)
+
+    def Fit_ROCurve_from_Stress_Strain_Data(self):
+        pass
+
+    def Fit_ROCurve_from_YS_UTS(self):
+        pass
+
+    def Fit_JRCurve_from_Data(self):
+        pass
+
+    def new_analysis(self):
+        #this is an code example code to show how to get value and set value.
+        normal_pipe_size = self.Normal_Pipe_Size.get()
+        print(normal_pipe_size)
+        #how to clear the entry
+        self.Normal_Pipe_Size.delete(0,tk.END)
+        #how to set the value of entry
+        self.Normal_Pipe_Size.insert(0,"123")
+        
+        pass
+
+    def open_analysis(self):
+        pass
+
+    def save_analysis(self):
+        pass
+
+    def run_analysis(self):
+        pass
+    
+    def run2_analysis(self):
+        pass
+
 
     def import_data(self):
         file_name = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
